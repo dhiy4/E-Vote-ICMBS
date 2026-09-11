1ca25d7e6826676ea5c50c093bf584e0395203df
dhiy4 <87604568+dhiy4@users.noreply.github.com>
2026-09-12 00:53:35 +0700

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    $('#dataTable').DataTable()
+    $('.scroll').click(function (e) {
+        e.preventDefault()
+        $('body, html').animate({
+            scrollTop: $(this.hash).offset().top
+        }, 1000)
+    })
+    $('.custom-file-input').on('change', function () {
+        let fileName = $(this).val().split('\\').pop();
+        $(this).next('.custom-file-label').addClass("selected").html(fileName);
+    })
+
+    $('#info').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget) // Button that triggered the modal
+        var recipient = button.data('whatever') // Extract info from data-* attributes
+        // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+        // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+        var modal = $(this)
+        modal.find('.modal-title').text('New message to ' + recipient)
+        modal.find('.modal-body input').val(recipient)
+    })
+
+})
